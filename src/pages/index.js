caf0371aa6a1f2e50c595056091b79bea810b360
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-09-20 15:34:07 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi all.</h1>
+    <p>My name is Tadas. I'm a developer-solopreneur from out of town. Conveniently staying in SF for a month right
+    when the hackathon and JAMStack_conf is going on!</p>
+    <p>Looking forward to making new friends and hacking away!</p>
+  </Layout>
+)
+
+export default IndexPage
